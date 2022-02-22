0d33e6fbb3b5809baec1819074fcbb9842a15d97
fz <34762675+faizalzafri@users.noreply.github.com>
2022-02-22 10:56:58 +0530

    day 19

---
+++ 19 - interfaces/__init__.py
@@ -0,0 +1,19 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        div_sum = 0;
+        for divisor in range(1, n):
+            if n % divisor == 0:
+                div_sum += divisor
+        return  div_sum + n
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
